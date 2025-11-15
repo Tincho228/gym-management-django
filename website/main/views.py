9169dc511a2e6333a415a538469d0bfcf607f282
Ailen Mansilla <man22007@byui.edu>
2025-11-15 02:02:39 -0500

    create templates and basic design. Update views and urls. Minimal updates to readme file.

--- website/main/views.py
+++ website/main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def dashboard(request):
+    return render(request, 'main/dashboard.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def login_view(request):
+    return render(request, 'main/login.html')

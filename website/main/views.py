0cf805098ac7e1921459fd722783a9991fa8cb93
jhtr0226 <johanncraft17@gmail.com>
2025-11-15 23:11:56 -0600

    Fix ROOT_URLCONF and URL includes for nested structure

--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+#
+## Create your views here.
+#def home(request):
+#    return render(request, 'main/home.html')
 
-# Create your views here.
+from django.http import HttpResponse
+
 def home(request):
-    #return render(request, 'main/home.html')
-    return HttpResponse("Gym Management App is Live!")
+    return HttpResponse("Gym Management App is Live! 🎉")

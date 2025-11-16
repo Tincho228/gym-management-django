535d76f7f77a8158720b8b9e9f52584c4d17b261
jhtr0226 <johanncraft17@gmail.com>
2025-11-16 00:40:07 -0600

    Add django_heroku configuration for production

--- website/website/settings.py
+++ website/website/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import dj_database_url
 from decouple import config
 
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

bdf7ff9f7eec87e8d1eb9a61938363d2383224fd
Johann Tellez <131574241+jhtr0226@users.noreply.github.com>
2025-11-18 21:24:04 -0600

    Remove home view and HttpResponse import

--- website/main/views.py
+++ website/main/views.py
@@ -3,8 +3,3 @@
 ## Create your views here.
 #def home(request):
 #    return render(request, 'main/home.html')
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Gym Management App is Live!")
